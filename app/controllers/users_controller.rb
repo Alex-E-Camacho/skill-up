099667088995911966e969b5841b79de27e3f6f4
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-06-11 10:15:24 -0400

    Enable update of profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+      if @user.update_attributes(user_params)
+        flash[:success] = "Profile updated"
+        redirect_to @user
+      else
+        @errors = @user.errors.full_messages
+        render 'edit'
+      end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :birthdate, :first_name, :last_name, :email, :password, :password_confirmation)
